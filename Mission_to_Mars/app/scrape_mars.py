173d8a2d0baad083c4022043a0163bcd84dacaaf
emansuetti <emansuetti@yahoo.com>
2021-07-09 19:53:03 -0700

    Code manipulation for scrape.ap

---
+++ Mission_to_Mars/app/scrape_mars.py
@@ -0,0 +1,24 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+            "news_title": news_title,
+            "news_paragraph": news_paragraph,
+            "featured_image": featured_image(browser),
+            "facts": mars_facts(),
+            "hemispheres": hemispheres(browser),
+            "last_modified": dt.datetime.now()
+        }
+    browser.quit()
+    return data
+
